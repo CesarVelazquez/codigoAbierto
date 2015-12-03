ddc52ba5927dc5a686ee56d971c0bf04db774297
Cesar <cesar43f@hotmail.com>
2015-12-02 20:04:18 -0600

    validacion

--- application/views/inicio.php
+++ application/views/inicio.php
@@ -5,10 +5,10 @@
         ?>
     <div class="col-sm-6 col-md-4">
         <div class="thumbnail">
-          <img src="http://lorempixel.com/400/200/people" alt="Evento">
+            <img src="<?php echo base_url('assets/img/'.$evento->foto) ?>" alt="Evento">
           <div class="caption">
             <h3><?php echo $evento->nombre ?></h3>
-            <p><?php echo $evento->descripcion ?></p>
+            <p class="text-justify"><?php echo $evento->descripcion ?></p>
             <p><a href="<?php echo site_url('ctrlEvento/getEvento/'.$evento->idEvento) ?>" class="btn btn-primary" role="button">Ver Detalle</a></p>
           </div>
         </div>
